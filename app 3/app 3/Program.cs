1dd8944ef5a820a4dd8c9d62c55424e91547d851
Ugnius <ugniusnaujokas@gmail.com>
2019-12-20 18:40:37 +0200

    Completed App 3

---
+++ app 3/app 3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace app_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String tekstas = "C#Kursas";
+            for (int i = 0; i < tekstas.Length; i++)
+            {
+                char a = tekstas[i];
+                Console.WriteLine(a);
+            }
+            Console.Read();
+        }
+    }
+}
