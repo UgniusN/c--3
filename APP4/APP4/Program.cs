73ae1de51ba9b1c48180e2459819e3c81cfb5f87
Ugnius <ugniusnaujokas@gmail.com>
2019-12-20 18:44:01 +0200

    Completed APP4

---
+++ APP4/APP4/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APP4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i<10; i++)
+            {
+                Thread.Sleep(100);
+                Console.WriteLine("#");
+            }
+            Console.WriteLine();
+            Console.WriteLine("Pabaiga");
+            Console.Read();
+        }
+    }
+}
