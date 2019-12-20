10e5e88bc84b5e9adf9461f0da6eb4506f482d6a
Ugnius <ugniusnaujokas@gmail.com>
2019-12-20 20:56:56 +0200

    Completed App14

---
+++ App14/App14/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] skaiciai = new int[5];
+            int[] kopija = new int[10];
+            String a;
+            int b;
+            int suma = 0;
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Iveskite skaiciu: ");
+                a = Console.ReadLine();
+                b = Convert.ToInt32(a);
+                skaiciai[i] = b;
+            }
+            for(int i=0; i<5; i++)
+            {
+                kopija[i] = skaiciai[i];
+            }
+            Console.WriteLine("");
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine(kopija[i]);
+            }
+            Console.Read(); 
+        }
+    }
+}
