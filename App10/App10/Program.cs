9ef9eb423170d0616a941901f8159e3f6c4256f3
Ugnius <ugniusnaujokas@gmail.com>
2019-12-20 20:03:52 +0200

    Completed App10

---
+++ App10/App10/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] savaitesDienos =
+            {
+                "pirmadienis",
+                "antradienis",
+                "treciadienis",
+                "ketvirtadienis",
+                "penktadienis",
+                "sestadienis",
+                "sekmadienis" 
+            };
+
+            for (int i=0; i<savaitesDienos.Length; i++)
+            {
+                Console.WriteLine(savaitesDienos[i]);
+            }
+            Console.Read();
+        }
+    }
+}
