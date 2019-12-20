c103baae4f2b3c01a67d8a05103f6c9dd4a7b961
Ugnius <ugniusnaujokas@gmail.com>
2019-12-20 20:28:24 +0200

    Completed App11

--- App11/App11/Program.cs
+++ App11/App11/Program.cs
@@ -10,6 +10,30 @@ namespace App11
     {
         static void Main(string[] args)
         {
+            string[] savaitesDienos =
+{
+                "pirmadienis",
+                "antradienis",
+                "treciadienis",
+                "ketvirtadienis",
+                "penktadienis",
+                "sestadienis",
+                "sekmadienis"
+            };
+
+            foreach(var diena in savaitesDienos)
+            {
+                Console.WriteLine(diena);
+            }
+            savaitesDienos[2] = "TRECIADIENIS";
+            Console.WriteLine("");
+            Console.WriteLine("Po:");
+
+            foreach(var diena in savaitesDienos)
+            {
+                Console.WriteLine(diena);
+            }
+            Console.Read();
         }
     }
 }
